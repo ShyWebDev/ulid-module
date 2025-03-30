e5c7a441c3c314cac7f3ba7145bc12245aaee2d3
ShyWebDev <c1216sg@hanmail.net>
2025-03-30 17:19:26 +0900

    등록 테스트

--- src/main/java/com/dev/hobby/ulid/UliGenerate.java
+++ src/main/java/com/dev/hobby/ulid/UliGenerate.java
@@ -3,7 +3,7 @@ package com.dev.hobby.ulid;
 import com.github.f4b6a3.ulid.UlidCreator;
 import jakarta.enterprise.context.ApplicationScoped;
 
-@ApplicationScoped
+
 public class UliGenerate {
 
     public String generate() {
